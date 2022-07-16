4bd96cd95be84d2e1a2da380e31ad1423e998dae
StevenMartinez94 <albertstevenmf@hotmail.com>
2022-07-16 16:41:09 -0400

    First quater done

---
+++ Todo-List/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+
+
+export default function App() {
+  return (
+    <View style ={styles.container}>
+
+      <View style = {styles.taskWrapper}>
+        <Text style = {styles.sectionTitle}>Today's task</Text>
+
+        <View style = {styles.items}>
+          <Task/>
+        </View>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+
+  items: {
+
+  }
+
+});
